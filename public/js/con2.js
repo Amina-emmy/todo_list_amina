c43274d8bb4400b52cd6ab9893a8a22987d00d31
Amina-emmy <amina.barghoute2001@gmail.com>
2023-06-09 13:40:38 +0100

    consigne 2 done SANS Bonus :o

--- public/js/con2.js
+++ public/js/con2.js
@@ -1,11 +1,123 @@
 let input = document.querySelector("#task");
 let send = document.querySelector("#send");
 let div_todo = document.querySelector("#todo");
+let div_doing = document.querySelector("#doing");
+let div_done = document.querySelector("#done");
 
 
+const create_icons_select = (divParent, p) => {
 
-//^ create 3 icons + their functions
-const create_icons = (divParent, p) => {
+    //^ create Select of each task + functions due to its choosen link
+    // todo : create by js div > btn & ul > li > a
+    //& div
+    let divS = document.createElement("div");
+    divParent.appendChild(divS);
+    divS.classList.add("btn-group");
+    divS.style.display = "flex";
+    //& btn
+    let btnS = document.createElement("button");
+    btnS.classList.add("btn", "btn-secondary", "dropdown-toggle", "btn-sm");
+    btnS.setAttribute("type", "button");
+    btnS.setAttribute("data-bs-toggle", "dropdown");
+    btnS.setAttribute("aria-expanded", "false");
+
+    let btnS_txt = document.createTextNode("Replace");
+    btnS.appendChild(btnS_txt);
+    divS.appendChild(btnS);
+
+    //& ul
+    let ulS = document.createElement("ul");
+    ulS.classList.add("dropdown-menu");
+    divS.appendChild(ulS);
+
+    //& li1 => to do
+    let li1 = document.createElement("li");
+    ulS.appendChild(li1);
+    let a1 = document.createElement("a");
+    a1.classList.add("dropdown-item");
+    a1.style.display = "none";
+    let a1_txt = document.createTextNode("To Do");
+    a1.appendChild(a1_txt);
+    li1.appendChild(a1);
+
+    //& li2 => doing
+    let li2 = document.createElement("li");
+    ulS.appendChild(li2);
+    let a2 = document.createElement("a");
+    a2.classList.add("dropdown-item");
+    let a2_txt = document.createTextNode("Doing");
+    a2.appendChild(a2_txt);
+    li1.appendChild(a2);
+
+    //& li1 => done
+    let li3 = document.createElement("li");
+    ulS.appendChild(li3);
+    let a3 = document.createElement("a");
+    a3.classList.add("dropdown-item");
+    let a3_txt = document.createTextNode("Done");
+    a3.appendChild(a3_txt);
+    li1.appendChild(a3);
+
+    // todo :  click on link to choose column => set task on that column
+    a1.addEventListener("click", () => { //^ click to do ==> from doing 
+        if (div_doing.id == "doing") {
+            div_doing.removeChild(p);
+            a1.style.display = "none";
+            a2.style.display = "block";
+            div_todo.appendChild(divS);
+            div_todo.appendChild(p);
+        }
+    });
+    a1.addEventListener("click", () => { //^ click to do ==> from done
+        if (div_done.id == "done") {
+            div_done.removeChild(p);
+            a1.style.display = "none";
+            a3.style.display = "block";
+            div_todo.appendChild(divS);
+            div_todo.appendChild(p);
+        }
+    });
+    a2.addEventListener("click", () => { //^ click doing ==> from to do
+        if (div_todo.id == "todo") {
+            div_todo.removeChild(p);
+            a1.style.display = "block";
+            a2.style.display = "none";
+            div_doing.appendChild(divS);
+            div_doing.appendChild(p);
+        }
+    });
+    a2.addEventListener("click", () => { //^ click doing ==> from done
+        if (div_done.id == "done") {
+            div_done.removeChild(p);
+            a2.style.display = "none";
+            a1.style.display = "block";
+            a3.style.display = "block";
+            div_doing.appendChild(divS);
+            div_doing.appendChild(p);
+        }
+    });
+    a3.addEventListener("click", () => { //^ click done ==> from doing 
+        if (div_doing.id == "doing") {
+            div_doing.removeChild(p);
+            a1.style.display = "block";
+            a2.style.display = "block";
+            a3.style.display = "none";
+            div_done.appendChild(divS);
+            div_done.appendChild(p);
+        }
+    });
+    a3.addEventListener("click", () => { //^ click done ==> from to do
+        if (div_todo.id == "todo") {
+            div_todo.removeChild(p);
+            a1.style.display = "block";
+            a2.style.display = "block";
+            a3.style.display = "none";
+            div_done.appendChild(divS);
+            div_done.appendChild(p);
+        }
+    });
+
+    //^ create 2 icons + their functions
     //? delete icon
     let ico_rm = document.createElement("i");
     ico_rm.classList.add("fa-solid", "fa-sm", "fa-circle-minus");
@@ -14,8 +126,23 @@ const create_icons = (divParent, p) => {
     p.appendChild(ico_rm);
 
     ico_rm.addEventListener("click", () => {
-        divParent.removeChild(p);
-    })
+        if (div_todo.id == "todo") {
+            div_todo.removeChild(p);
+            div_todo.removeChild(divS);
+        }
+    });
+    ico_rm.addEventListener("click", () => {
+        if (div_doing.id == "doing") {
+            div_doing.removeChild(p);
+            div_doing.removeChild(divS);
+        }
+    });
+    ico_rm.addEventListener("click", () => {
+        if (div_done.id == "done") {
+            div_done.removeChild(p);
+            div_done.removeChild(divS);
+        }
+    });
 
     //? modify icon
     let ico_modify = document.createElement("i");
@@ -24,31 +151,54 @@ const create_icons = (divParent, p) => {
     ico_modify.style.paddingInlineEnd = "15px";
     p.appendChild(ico_modify);
 
-    //! when i modify icons disappear ?
-    ico_modify.addEventListener("click", () => { 
+    ico_modify.addEventListener("click", () => {
         let makeSure = confirm(`sure you wanna modify this task "${p.textContent} "`);
         if (makeSure == true) {
             let askModif = prompt(`task "${p.textContent}" Change it to --> `);
-            p.textContent = askModif;
-            alert("task will be modified");
+            if (p.textContent != askModif) {
+                // when i modify p.textContent icons disappear, so i put icons after p.textContent
+                p.textContent = askModif;
+                p.appendChild(ico_rm);
+                p.appendChild(ico_modify);
+                alert("task will be modified");
+            } else {
+                alert("you did not really modify anything");
+            }
         }
     });
-
-    //& select 
-
 }
 
 //* create task <p> with its icons + select
-const createTask_todo = () => {
+const createTask = (div_wanted) => {
     //? create <p> contains input.value
     let para = document.createElement("p");
     para.style.fontWeight = "bold";
+    para.style.fontSize = "18px";
     let node = document.createTextNode(input.value);
-    create_icons(div_todo, para);
+    create_icons_select(div_wanted, para);
     para.appendChild(node);
-    div_todo.appendChild(para);
+    div_wanted.appendChild(para);
 }
 
+//* addEventListener to the btn & the key Enter
 send.addEventListener("click", () => {
-    createTask_todo();
+    //! missing : condition if task alredy existed
+    if (input.value == "") {
+        alert("you did not write anything");
+    } else {
+        createTask(div_todo);
+        input.value = "";
+    }
+});
+
+input.addEventListener("keyup", (event) => { //? en click sur Enter
+    if (event.key == "Enter") {
+        //! missing : condition if task alredy existed
+        if (input.value == "") {
+            alert("you did not write anything");
+        } else {
+            createTask(div_todo);
+            input.value = "";
+        }
+    }
 });
